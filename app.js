ae1e453c2d68f5ece54a135ce89d06d915e9b4e2
SupriyaK31 <supriyakittur31@gmail.com>
2024-01-26 12:42:12 +0530

    User

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const Product=require('./models/product');
+const User=require('./models/user');
 // db.execute('Select * from products')
 // .then((result)=>{console.log(result)})
 // .catch((err)=>{console.log(err)});
@@ -22,9 +24,23 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+
+Product.belongsTo(User,{constraints:true, onDelete:'CASCADE'});
+User.hasMany(Product);
+
 sequelize.sync()
 .then((result)=>{
     // console.log(result);
+    return User.findByPK(1);
+    // app.listen(3000);
+}).then(user=>{
+    if(!user){
+        return User.create({name:'max',enail:'test@admin.com'});
+    }
+    return user;
+})
+.then(user=>{
+    console.log(user);
     app.listen(3000);
 })
 .catch((err)=>{
