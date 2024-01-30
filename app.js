78e7ff64c85d7811d6b69da11c06db14ae179aab
SupriyaK31 <supriyakittur31@gmail.com>
2024-01-31 00:03:00 +0530

    deletefun

--- app.js
+++ app.js
@@ -6,41 +6,55 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const sequelize=require('./util/database');
 const app = express();
+const Product=require('./models/product');
+const User=require('./models/user');
+const Cart=require('./models/card');
+const cartItem=require('./models/cartItem');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const Product=require('./models/product');
-const User=require('./models/user');
 // db.execute('Select * from products')
 // .then((result)=>{console.log(result)})
 // .catch((err)=>{console.log(err)});
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use((req,res,next)=>{
+    User.findByPk(1)
+    .then(user=>{
+        req.user=user;
+        next();
+    })
+    .catch(err=>{ console.log(err)});
+});
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
 app.use(errorController.get404);
 
-Product.belongsTo(User,{constraints:true, onDelete:'CASCADE'});
+Product.belongsTo(User,{ constraints:true, onDelete:'CASCADE'});
 User.hasMany(Product);
-
-sequelize.sync()
+Cart.hasOne(User);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product,{ through : cartItem});
+Cart.belongsToMany(User,{ through:cartItem});
+sequelize
+.sync()
+// .sync({force:true})
 .then((result)=>{
-    // console.log(result);
-    return User.findByPK(1);
-    // app.listen(3000);
-}).then(user=>{
+    return User.findByPk(1);
+})
+.then((user)=>{
     if(!user){
-        return User.create({name:'max',enail:'test@admin.com'});
+        return User.create({ name: 'Max', email: 'test@admin.com' });
     }
     return user;
+}).then((user)=>{
+    return user.createCart();
 })
-.then(user=>{
-    console.log(user);
+.then((cart)=>{
+    //  console.log(user);
     app.listen(3000);
 })
 .catch((err)=>{
